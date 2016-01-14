6006f0c60cca7b6df240309b1022dcef1a657d5c
Nguyễn Tấn Tin <nguyentantintb@gmail.com>
2016-01-14 20:04:24 +0700

    update ModelFactory

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -9,13 +9,28 @@
 | you a convenient way to create models for testing and seeding your
 | database. Just tell the factory how a default model should look.
 |
-*/
+ */
 
 $factory->define(App\User::class, function (Faker\Generator $faker) {
-    return [
-        'name' => $faker->name,
-        'email' => $faker->email,
-        'password' => bcrypt(str_random(10)),
-        'remember_token' => str_random(10),
-    ];
+	return [
+		'name' => $faker->name,
+		'email' => $faker->email,
+		'password' => \Hash::make('123456'),
+		'remember_token' => str_random(10),
+	];
+});
+
+$factory->define(App\Category::class, function (Faker\Generator $faker) {
+	return [
+		'name' => $faker->sentence(3),
+	];
+});
+
+$factory->define(App\Category::class, function (Faker\Generator $faker) {
+	return [
+		'name' => $faker->word,
+		'price' => 12000,
+		'description' => $faker->sentence(5),
+		'category_id' => rand(1, 6),
+	];
 });
